f38dcbf887b49aabee0f6ad091720dabc81f6090
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2023-08-12 01:09:02 -0500

    feat: add topics screen

--- lib/presentation/screens/topic/units_screen.dart
+++ lib/presentation/screens/topic/units_screen.dart
@@ -1,43 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class UnitsScreen extends StatelessWidget {
   const UnitsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Algebra Lineal'),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: const Text('Vectores'),
-            subtitle: const Text('Aprende los vectores'),
-            leading: const Icon(Icons.numbers_rounded),
-            trailing: PopupMenuButton(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  child: const Text('Playlist'),
-                  onTap: () {
-                    context.push(
-                      '/units/:unitId/playlist',
-                      extra: {'unitId': '1'},
-                    );
-                  },
-                ),
-                PopupMenuItem(
-                  child: const Text('Jugar'),
-                  onTap: () {
-                    context.push('/quiz');
-                  },
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
