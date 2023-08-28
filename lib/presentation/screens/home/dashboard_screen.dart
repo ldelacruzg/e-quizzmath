00df4fce400e3253fe07eda890f943e80140dfc7
USER <ebertguaranga@gmail.com>
2023-08-28 17:17:15 -0500

    Home

--- lib/presentation/screens/home/dashboard_screen.dart
+++ lib/presentation/screens/home/dashboard_screen.dart
@@ -5,6 +5,15 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/welcome_messages/1.png'),
+          const Text('Bienvenidos a e-QuizzMath')
+        ],
+      ),
+    ));
   }
 }
