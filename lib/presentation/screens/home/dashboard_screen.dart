d8ad5d5149944c8d8068ac3f6db6a91c492d88f7
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2023-09-01 01:30:28 -0500

    feat: change home design

--- lib/presentation/screens/home/dashboard_screen.dart
+++ lib/presentation/screens/home/dashboard_screen.dart
@@ -5,15 +5,80 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/welcome_messages/1.png'),
-          const Text('Bienvenidos a e-QuizzMath')
-        ],
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _CustomItemAction(
+                  color: Colors.purpleAccent.shade700,
+                  text: 'Unirse',
+                  icon: Icons.school_rounded,
+                ),
+                _CustomItemAction(
+                  color: Colors.greenAccent.shade700,
+                  text: 'Quiz',
+                  icon: Icons.quiz_rounded,
+                ),
+                _CustomItemAction(
+                  color: Colors.orangeAccent.shade700,
+                  text: 'Puntuación',
+                  icon: Icons.score_rounded,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+}
+
+class _CustomItemAction extends StatelessWidget {
+  final Color color;
+  final String text;
+  final IconData icon;
+
+  const _CustomItemAction({
+    required this.color,
+    required this.text,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Card(
+      elevation: 10,
+      child: Container(
+        width: size.width * 0.3 - (40 * 100 / size.width),
+        height: (size.width * 0.3) * 1.5,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.white, size: 50),
+            const SizedBox(height: 10),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:e_quizzmath/presentation/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:random_avatar/random_avatar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,8 +20,9 @@ class _HomeScreenState extends State<HomeScreen> {
     final leaderProvider = context.watch<LeaderboardProvider>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('e-QuizzMath'),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: _CustomAppBar(),
       ),
       body: [
         const DashboardScreen(), // Dashboard
@@ -59,6 +61,62 @@ class _HomeScreenState extends State<HomeScreen> {
             label: 'Perfil',
             icon: Icon(Icons.person_2_rounded),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return AppBar(
+      // ignore: prefer_const_constructors
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(radius: 20, child: RandomAvatar('Luis De La Cruz')),
+              const SizedBox(width: 10),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hola,',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    'Luis De La Cruz',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            decoration: BoxDecoration(
+              color: colorScheme.primary,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: const Text(
+              '120 EXP',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
+            ),
+          )
         ],
       ),
     );
